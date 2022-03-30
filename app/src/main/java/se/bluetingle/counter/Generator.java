62998818a4eb030e993e43480801b1ec3f8ca089
Bluetingle <96061625+Bluetingle@users.noreply.github.com>
2022-03-30 22:24:06 +0200

    Lastest build

--- app/src/main/java/se/bluetingle/counter/Generator.java
+++ app/src/main/java/se/bluetingle/counter/Generator.java
@@ -9,56 +9,56 @@ public class Generator {
         boolean spela = true;
         String fortsatt;
 
-        Generator g = new Generator();
+        Generator g = new Generator();                                                // Instansiering av nytt objekt
+//      Looper så länge man väljer att fortsätta
         while (spela = true) {
             try {
-                g.guess();
+                g.guess();                                                            // Anropar guess metoden
             } catch (Exception e) {
                 System.out.println("Potatisskalle inga decimaltal!!!!!!!! eller bokstaver");
             }
-            Scanner svar = new Scanner(System.in);
+            Scanner svar = new Scanner(System.in);                                    // Nytt scanner objekt för att hämta in svar på om man vill fortsätta
             System.out.println("Spela igen? J/N" );
-                fortsatt= svar.nextLine();
-                    if(fortsatt.equalsIgnoreCase("J")){
+                fortsatt= svar.nextLine();                                            // svar inhämtas från tangentbordsinmatning
+                    if(fortsatt.equalsIgnoreCase("J")){                    // Om svar j så sätts fortsatt till true
                         spela = true;}
-                    else if (fortsatt.equalsIgnoreCase("N")){
+                    else if (fortsatt.equalsIgnoreCase("N")){              // Om svar nej så sätt fortsatt till false
                         spela = false;
                         System.out.println("Tack för att du spelade!");
-                        break;
+                        break;                                                        // Avbryter programmet
                     }
         }
     }
-    // Method to implement to return modified String
+
+    // Metod att returnera en modifierad String
     static String extractInt(String str)
     {
-        str = str.replaceAll("[^\\d]", " ");
-        str = str.trim();
+        str = str.replaceAll("[^\\d]", " ");                          // Byter ut alla icke tecken som inte är siffror mellan 0-9 mot en blankrad
+        str = str.trim();                                                             // Tar bort alla blankrader
         str = str.replaceAll(" +", " ");
-        if (str.equals(""))
-            return "-1";
-
-        return str;
+        if (str.equals(""))                                                           // Om strängen är tom
+            return "-1";                                                              // Returnera värdet -1
+        return str;                                                                   // Returnerar strängen
     }
     public void guess () {
-        int ranint = (new Random()).nextInt(100) + 1;
+        int ranint = (new Random()).nextInt(100) + 1;                           // Genererar ett tal mella 1 och 100
         String antal ;
         int fromstring;
 
-        Scanner myObj = new Scanner(System.in);
+        Scanner myObj = new Scanner(System.in);                                       // instansiering av nytt scannerobject
         System.out.println("Gissa nummer mellan 1 och 100:");
+//
+//      Kör denna loop så länge som fromint inte matchar ranint
         do {
-            antal = myObj.nextLine();
-            antal = extractInt(antal);
-            fromstring = Integer.parseInt(antal);
-            if (fromstring > ranint)
-                System.out.println("Du gissade för högt! Försök igen! :)"); //Hjälper spelaren att hitta rätt svar
+            antal = myObj.nextLine();                                                 // "antal" sätts till nästa rad som matas in från tangentbordet
+            antal = extractInt(antal);                                                // kallar på metoden "extractint" för att få tillbaka en textsträng med bara siffror
+            fromstring = Integer.parseInt(antal);                                     // Parsar strängen "antal" till en integer (omvandlar från String till int)
+            if (fromstring > ranint)                                                  // För högt
+                System.out.println("Du gissade för högt! Försök igen! :)");           // Hjälper spelaren att hitta rätt svar
             else
             if (fromstring < ranint)
-                System.out.println("Du gissade för lågt! Försök igen! :)"); //Hjälper spelaren att hitta rätt svar
-            //            if (fromstring != ranint) {
-//                System.out.println("Du gissade: " + antal + " Det är fel, prova igen!");
-//            }
+                System.out.println("Du gissade för lågt! Försök igen! :)");          //Hjälper spelaren att hitta rätt svar
         } while (fromstring != ranint);
-        System.out.println("Grattis du gissade rätt!");
+        System.out.println("Grattis du gissade rätt! rätt svar var "+ ranint);       // Om man gissat rätt!
     }
 }
--- app/src/test/java/se/bluetingle/counter/GeneratorTest.java
+++ app/src/test/java/se/bluetingle/counter/GeneratorTest.java
@@ -1,21 +1,65 @@
 package se.bluetingle.counter;
 
+import static org.junit.Assert.*;
+import static se.bluetingle.counter.Generator.extractInt;
+
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.sql.SQLOutput;
+import java.util.Random;
+import java.util.Scanner;
 
 public class GeneratorTest {
 
-//should generate random number between 1 and 100
+    public static void main (String[] args){
+
+    }
 
     @Test
+    // Att ett tal mellan 1-100 genereras
     public void shouldGenerateNumber() throws Exception{
+            int random = (new Random()).nextInt(100)+1;         // Genererar ett tal mellan 1 och 100
+            int highest = 100;
+            int lowest = 1;
+            assertTrue("Error, random is too high", highest >= random); // Är värdet högre än tillåtet
+            assertTrue("Error, random is too low",  lowest  <= random); // Är värdet lägre än tillåtet
+            System.out.println("Test passed: " + random + " is within " + lowest + " and " + highest); // Värdet är inom gränsvärden
+    }
+    @Test
+    public void shouldConvertToInt() throws Exception {
+        // Att fromstring konventerar till integer
+        String fromstring = "11";                                   // Strängen sätts till 11
+
+        try {
+            int parsed = Integer.parseInt(fromstring);              // Parsar från sträng till integer
+            Integer.valueOf(parsed);                                // Sätter värdet till samma som värdet på integer "parsed". Om "parsed" inte är en integer kommer det inte att gå
+            System.out.println(parsed + " är en integer!");
+        } catch (NumberFormatException e) {                         // Om "parsed" inte är en integer fånga felet
+            Assert.fail("Value '" + fromstring + " is not an Integer");
+        }
 
     }
 
-    public void shouldBe1() throws Exception{
-
+    @Test
+    public void shouldBeAString() throws Exception {
+// Att guess tar emot en sträng
+        String antal = "null";
+//      Kollar att det är en sträng
+        if (antal != null) {                                // Om "antal" inte är null så är det en sträng
+            System.out.println(antal + " Är en sträng");
+            } else                                          // Om antal är null så är det inte en sträng
+            {
+                System.out.println(antal + " är inte en sträng");
+            }
     }
 
-    public void shouldBeAString() throws Exception{
-
+    @Test
+    public void  extractIntModifiesString() throws Exception{
+        // Att extractint modifierar strängen
+        String unmodifiedstr = "test123";                   // Sätter strängen till test123
+        String modified = extractInt(unmodifiedstr);        // Anropar "extractInt" för att modifiera strängen
+        System.out.println("Strängen som den ser ut från början: " + unmodifiedstr);
+        System.out.println("Strängen ser nu ut så här: "+modified);     // Skriver ut den moddifierade strängen
     }
 }
